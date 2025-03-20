e38a2232d14a3df7a8105ee36ea14b56b389b4a0
ducdt2003 <dinhtrongduc2003@gmail.com>
2025-03-20 15:14:08 +0700

    btvn buoi 18

---
+++ buoi_18/movie-app/src/main/java/com/example/movieapp/controller/web/WebController.java
@@ -0,0 +1,60 @@
+package com.example.movieapp.controller.web;
+
+import com.example.movieapp.entity.Movie;
+import com.example.movieapp.model.enums.MovieType;
+import com.example.movieapp.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class WebController {
+    private final MovieService movieService;
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/phim-bo")
+    public String getPhimBoPage(@RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "18") Integer pageSize,
+                                Model model) {
+        Page<Movie> moviePage = movieService.findByType(MovieType.PHIM_BO, true, page, pageSize);
+        model.addAttribute("moviePage", moviePage);
+        model.addAttribute("currentPage", page);
+        return "phim-bo";
+    }
+    @GetMapping("/phim-le")
+    public String getPhimLePage(@RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "18") Integer pageSize,
+                                Model model) {
+        Page<Movie> moviePage = movieService.findByType(MovieType.PHIM_LE, true, page, pageSize);
+        model.addAttribute("moviePage", moviePage);
+        model.addAttribute("currentPage", page);
+        return "phim-le";
+    }
+
+    @GetMapping("/phim-chieu-rap")
+    public String getPhimChieuRapPage(@RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "18") Integer pageSize,
+                                Model model) {
+        Page<Movie> moviePage = movieService.findByType(MovieType.PHIM_CHIEU_RAP, true, page, pageSize);
+        model.addAttribute("moviePage", moviePage);
+        model.addAttribute("currentPage", page);
+        return "phim-chieu-rap";
+    }
+
+        @GetMapping("/phim/{id}/{slug}")
+        public String getMovieDetailsPage(@PathVariable Integer id, @PathVariable String slug) {
+            return "chi-tiet-phim";
+
+        }
+
+        }
